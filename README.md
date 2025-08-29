9ebfaafca22b6063f468500c354dc38e4ff76079
Sinenhlanhla Magubane <snenhlanhlamagubane99@gmail.com>
2025-08-29 10:24:51 +0200

    feat: consoling the data/characters individually.

---
+++ README.md
@@ -0,0 +1,25 @@
+### Running the JSON Server
+#### ```run``` this on the terminal (front-end)
+```$ npx json-server --watch src/data/dbz.json
+--watch/-w can be omitted, JSON Server 1+ watches for file changes by default
+JSON Server started on PORT :3000
+Press CTRL-C to stop
+Watching src/data/dbz.json...
+
+( ˶ˆ ᗜ ˆ˵ )
+
+Index:
+http://localhost:3000/
+
+Static files:
+Serving ./public directory if it exists
+
+Endpoints:
+http://localhost:3000/items
+http://localhost:3000/meta
+http://localhost:3000/links ```
+
+
+### front-end is for running & installing
+
+### back-end is for running the server
--- src/App.ts
+++ src/App.ts
@@ -1,15 +1,18 @@
-
 // @ts-ignore
-import axios from "https://cdn.jsdelivr.net/npm/axios@1.6.7/+esm"
+// import axios from "https://cdn.jsdelivr.net/npm/axios@1.6.7/+esm"
+let characters: any[] = [];
 async function fetchItems() {
   try {
     const response = await fetch("http://localhost:3000/items");
     const data = await response.json();
-    console.log(data); // This is your actual data
+    // console.log(data); // This is the actual data
+    characters = data;
+    console.log(characters[5]);
   } catch (error) {
     console.log("Error:", error);
   }
 }
-
 // Calling the function
 fetchItems();
+// console.log("hello");
+// console.log(characters);
